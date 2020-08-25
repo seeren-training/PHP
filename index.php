5df45ccf879538af6d21fdf9b867c160923ae496
seeren <consultant@seeren.fr>
2020-08-25 23:24:49 +0200

    Add session

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo filter_var($_GET["name"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+session_start([
+    "cache_limiter", "nocache",
+    "cookie_httponly" => 1,
+    "cookie_path" => "/",
+    "use_cookies" => 1,
+    "use_only_cookies" => 1,
+    "use_strict_mode" => 1,
+    "use_trans_sid" => 0,
+]);
+
+if (!array_key_exists("count", $_SESSION)) {
+    $_SESSION["count"] = 0;
+}
+
+echo ++$_SESSION["count"];
