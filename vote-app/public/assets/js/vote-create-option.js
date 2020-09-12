fee6c4c5cd76d8004674e30f40a75659a9ea517c
seeren <consultant@seeren.fr>
2020-09-12 16:45:04 +0200

    Correction

--- vote-app/public/assets/js/vote-create-option.js
+++ vote-app/public/assets/js/vote-create-option.js
@@ -3,7 +3,7 @@ document.querySelector('.add-option').addEventListener("click", () => {
         const optionGroup = document.createElement("div");
         optionGroup.setAttribute("class", "position-relative form-group");
         optionGroup.innerHTML = `
-                <label for="option_${length}">Option +</label>
+                <label for="option_${length}">Option</label>
                 <input class="vote-option col-9 form-control" id="option_${length}" name="optionList[]" placeholder="Option">
                 <span class="position-absolute remove-option btn btn-danger" style="bottom: 0; right: 0">x</span>`;
         document.querySelector(".vote-option-group")
