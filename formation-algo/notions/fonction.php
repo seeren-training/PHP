6b8d5b510aafc5f057fc51271093b3a61a11f497
seeren <consultant@seeren.fr>
2021-09-13 17:11:54 +0200

    Add function

---
+++ formation-algo/notions/fonction.php
@@ -0,0 +1,13 @@
+<?php
+
+$tab = [];
+
+// count est une fonction
+// Une fonction porte un nom (un identifiant)
+// Et porte des parenthèses d'execution
+// Les parenthèses formulent une liste d'arguments
+//echo count($tab);
+$age = readline("Quel age avez vous?");
+
+
+echo "Vous avez: $age";
